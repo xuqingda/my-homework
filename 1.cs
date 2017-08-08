09820d2d50b4fd984c7235ae6245a55cf7b142da
qingda xu <xuqingda123@gmail.com>
2017-08-08 21:28:52 +0900

    homework

---
+++ 1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int i;
+            int j=0;
+
+           // int e;
+            //int n = 100000;
+            int[] arry1 = new int[6] { 1, 2, 2, 3, 4, 5}; 
+
+            for ( i=0 ; i<6; i++)
+            {
+                if (arry1[i] == 2)
+                {
+                    i++;
+                }
+                arry1[j] = arry1[i];
+                j++;
+                
+            }
+            Console.Write(j);
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ 2.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //public int sort()
+            //{
+
+            //}
+            int n = 6;
+            int[] a = new int[6] { 1, 1, 2, 2, 3, 4 };
+
+            int j = 0;
+            int i = 0;
+            for (i = 1; i < n; i++)
+            {
+                
+                if (a[j] != a[i])
+                {
+                   
+                    a[++j] = a[i];
+                    
+
+                }
+
+            }
+
+            Console.WriteLine(j+1);
+
+            Console.ReadKey();
+        }
+
+    }
+
+}
+
